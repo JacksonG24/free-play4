58d158250826ab2e5ed744dc571d21ba29eb01bb
JACKSONG24 <JRGAMMACK24@BLAKESCHOOL.ORGm>
2023-10-16 22:59:24 -0500

    Initial Commit

---
+++ free play4/free_play4App.swift
@@ -0,0 +1,17 @@
+//
+//  free_play4App.swift
+//  free play4
+//
+//  Created by Jackson Gammack24 on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct free_play4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
