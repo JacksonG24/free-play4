a8a6c76aa99ce4040804c27c1916b402957a1848
JACKSONG24 <JRGAMMACK24@BLAKESCHOOL.ORGm>
2023-10-17 00:30:20 -0500

    Finished video three still some things are not working from the video

--- free play4/Antmation curves.swift
+++ free play4/Antmation curves.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct Antmation_curves: View {
+    let timing: Double = 10
+    @State var isAnimated: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Button("Button"){
+                isAnimated.toggle()
+            }
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: isAnimated ? 350 : 50, height: 100)
+                .animation(.spring(
+                    response: 3.0,
+                    dampingFraction: 1.0,
+                    blendDuration: 1.0
+                ))
+//                .animation(Animation.linear(duration: timing ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimated ? 350 : 50, height: 100)
+//                .animation(Animation.easeIn(duration: timing ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimated ? 350 : 50, height: 100)
+//                .animation(Animation.easeInOut(duration: timing ))
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimated ? 350 : 50, height: 100)
+//                .animation(Animation.easeIn(duration: timing ))
+     }
     }
 }
 
